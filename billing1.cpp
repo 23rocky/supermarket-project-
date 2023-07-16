cea7cf3f1cbbea2ee1785fb7ecfbe18acc06f45c
23rocky <137681460+23rocky@users.noreply.github.com>
2023-07-16 23:09:27 +0530

    Update supermarket.cpp

--- billing1.cpp
+++ billing1.cpp
@@ -54,7 +54,7 @@ void shopping :: menu()
 			cout<<"\t\t\t Password       \n";
 			cin>>password;
 			
-			if(email=="robby@email.com" && password=="robby@123")
+			if(email=="akshaj@email.com" && password=="akshaj@123")
 			{
 				administrator();
 			}
